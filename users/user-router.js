34e1a6bdcdd7200b4143665cf222fe8c51aea754
B Severino <biancasev@gmail.com>
2020-02-03 15:15:06 -0500

    Added user delete endpoint

--- users/user-router.js
+++ users/user-router.js
@@ -29,4 +29,21 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+
+    if (Number(id) === Number(req.user.id)) {
+        Users.remove(id)
+            .then(() => {
+                res.status(200).json({ message: 'User successfully deleted.' })
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({ message: 'Error deleting user.' })
+            })
+    } else {
+        res.status(401).json({ message: 'You do not have authorization to do this.' })
+    }
+})
+
 module.exports = router
